3f139105447573bb70ab200d0ad30b46aa5c59d2
ManitoYu <412950798@qq.com>
2016-06-12 16:11:55 +0800

    增加contentBase配置

--- server.js
+++ server.js
@@ -5,12 +5,13 @@ const webpack = require('webpack');
 const config = require('./webpack.config');
 
 let server = new WebpackDevServer(webpack(config), {
-  proxy: {
-    "*": "http://192.168.162.130:8000"
-  },
+  // proxy: {
+  //   "*": "http://192.168.1.112:8000"
+  // },
   hot: false,
   inline: true,
-  noInfo: true
+  noInfo: true,
+  contentBase: './src'
 });
 
-server.listen(3000, '192.168.162.130');
+server.listen(3000, '192.168.1.112');
