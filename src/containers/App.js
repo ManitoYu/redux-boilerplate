4b1c50ebf090eb5aa966fbd750167e2e3feab1bc
ManitoYu <412950798@qq.com>
2016-06-12 16:11:12 +0800

    修改App容器

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,10 +1,14 @@
 import React, { Component, PropTypes } from 'react'
 import { connect } from 'react-redux'
 
+import {
+  Button
+} from '../components'
+
 class App extends Component {
   render() {
     return (
-      <h1 onClick={() => this.props.dispatch({ type: 'ADD_POST', post: { id: Math.random() } })}>App</h1>
+      <Button onClick={() => alert()}>Click</Button>
     )
   }
 }
