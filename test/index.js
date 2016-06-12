f2aedf7b15b8eaf3ceaa810da5fc1f1a6294d29d
ManitoYu <412950798@qq.com>
2016-06-12 16:10:05 +0800

    增加test入口文件

---
+++ test/index.js
@@ -0,0 +1,9 @@
+import jsdom from 'jsdom'
+
+const doc = jsdom.jsdom('<!doctype html><html><body></body></html>')
+const win = doc.defaultView
+
+global.document = doc
+global.window = win
+
+import '../src/components/Button/Button.spec.js'
