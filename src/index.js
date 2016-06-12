8e5a6741b294c073c32011615c38a8d0434c2050
ManitoYu <412950798@qq.com>
2016-06-12 16:10:54 +0800

    修改应用入口文件

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import 'babel-polyfill'
+import './index.scss'
+
 import React from 'react'
 import { render } from 'react-dom'
-import { hashHistory } from 'react-router'
+import { browserHistory } from 'react-router'
 import configureStore from './store/configureStore'
 import Root from './containers/Root'
 
 const store = configureStore()
 
 render(
-  <Root store={store} history={hashHistory} />,
+  <Root store={store} history={browserHistory} />,
   document.querySelector('#root')
 )
